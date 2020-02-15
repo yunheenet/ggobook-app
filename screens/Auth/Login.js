ad2bf1efb62026d5ab7854a27c13e41fb13ef267
YunHee Lee <yunheenet@gmail.com>
2020-02-15 12:30:08 +0900

    ADD Confirm part

--- screens/Auth/Login.js
+++ screens/Auth/Login.js
@@ -41,11 +41,11 @@ export default ({ navigation }) => {
       } = await requestSecretMutation();
       if (requestSecret) {
         Alert.alert("Check your email");
-        navigation.navigate("Confirm");
+        navigation.navigate("Confirm", { email: value });
         return;
       } else {
         Alert.alert("Account not found");
-        navigation.navigate("Signup");
+        navigation.navigate("Signup", { email: value });
       }
     } catch (e) {
       Alert.alert("Can't log in now");
