7489f200868867f7084e5fb70bdc80e387cf732a
YunHee Lee <yunheenet@gmail.com>
2020-02-19 10:59:11 +0900

    ADD Post component header

--- screens/Tabs/Home.js
+++ screens/Tabs/Home.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import Loader from "../../components/Loaders";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
+import Post from "../../components/Post";
 
 const FEED_QUERY = gql`
   {
@@ -63,7 +64,13 @@ export default () => {
         <RefreshControl refreshing={refreshing} onRefresh={refresh} />
       }
     >
-      {loading ? <Loader /> : <Text>Hello</Text>}
+      {loading ? (
+        <Loader />
+      ) : (
+        data &&
+        data.seeFeed &&
+        data.seeFeed.map(post => <Post key={post.id} {...post} />)
+      )}
     </ScrollView>
   );
 };
