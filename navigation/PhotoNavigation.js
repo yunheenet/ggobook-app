e91b5baee2368e8386cac7cdf040543bed4479c1
YunHee Lee <yunheenet@gmail.com>
2020-02-19 13:02:12 +0900

    ADD Photo Tab styles

--- navigation/PhotoNavigation.js
+++ navigation/PhotoNavigation.js
@@ -5,21 +5,51 @@ import {
 import SelectPhoto from "../screens/Photo/SelectPhoto";
 import TakePhoto from "../screens/Photo/TakePhoto";
 import UploadPhoto from "../screens/Photo/UploadPhoto";
-import stackStyles from "./config";
+import { stackStyles } from "./config";
+import styles from "../styles";
 
 const PhotoTabs = createMaterialTopTabNavigator(
   {
-    SelectPhoto,
-    TakePhoto
+    SelectPhoto: {
+      screen: SelectPhoto,
+      navigationOptions: {
+        tabBarLabel: "Select"
+      }
+    },
+    TakePhoto: {
+      screen: TakePhoto,
+      navigationOptions: {
+        tabBarLabel: "Take"
+      }
+    }
   },
   {
-    tabBarPosition: "bottom"
+    tabBarPosition: "bottom",
+    tabBarOptions: {
+      indicatorStyle: {
+        backgroundColor: styles.blackColor,
+        marginBottom: 20
+      },
+      labelStyle: {
+        color: styles.blackColor,
+        fontWeight: "600"
+      },
+      style: {
+        paddingBottom: 20,
+        ...stackStyles
+      }
+    }
   }
 );
 
 export default createStackNavigator(
   {
-    PhotoTabs,
+    PhotoTabs: {
+      screen: PhotoTabs,
+      navigationOptions: {
+        header: null
+      }
+    },
     UploadPhoto
   },
   {
