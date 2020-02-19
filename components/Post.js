890402e574c0545902b5fd37c4c53b3e4d50d475
YunHee Lee <yunheenet@gmail.com>
2020-02-19 11:38:58 +0900

    ADD Swiper and finishing post component

--- components/Post.js
+++ components/Post.js
@@ -1,9 +1,23 @@
-import React from "react";
-import { Image } from "react-native";
+import React, { useState } from "react";
+import { Image, Platform } from "react-native";
 import styled from "styled-components";
+import { Ionicons } from "@expo/vector-icons";
 import PropTypes from "prop-types";
+import Swiper from "react-native-swiper";
+import { gql } from "apollo-boost";
+import constants from "../constants";
+import styles from "../styles";
+import { useMutation } from "@apollo/react-hooks";
 
-const Container = styled.View``;
+export const TOGGLE_LIKE = gql`
+  mutation toggelLike($postId: String!) {
+    toggleLike(postId: $postId)
+  }
+`;
+
+const Container = styled.View`
+  margin-bottom: 40px;
+`;
 const Header = styled.View`
   padding: 15px;
   flex-direction: row;
@@ -19,8 +33,52 @@ const Bold = styled.Text`
 const Location = styled.Text`
   font-size: 12px;
 `;
+const IconsContainer = styled.View`
+  flex-direction: row;
+  margin-bottom: 5px;
+`;
+const IconContainer = styled.View`
+  margin-right: 10px;
+`;
+const InfoContainer = styled.View`
+  padding: 10px;
+`;
+const Caption = styled.Text`
+  margin: 5px 0px;
+`;
+const CommentCount = styled.Text`
+  opacity: 0.5;
+  font-size: 13px;
+`;
 
-const Post = ({ user, location }) => {
+const Post = ({
+  id,
+  user,
+  location,
+  files = [],
+  likeCount: likeCountProp,
+  caption,
+  comments = [],
+  isLiked: isLikedProp
+}) => {
+  const [isLiked, setIsLiked] = useState(isLikedProp);
+  const [likeCount, setLikeCount] = useState(likeCountProp);
+  const [toggleLikeMutaton] = useMutation(TOGGLE_LIKE, {
+    variables: {
+      postId: id
+    }
+  });
+  const handleLike = async () => {
+    if (isLiked === true) {
+      setLikeCount(l => l - 1);
+    } else {
+      setLikeCount(l => l + 1);
+    }
+    setIsLiked(p => !p);
+    try {
+      await toggleLikeMutaton();
+    } catch (e) {}
+  };
   return (
     <Container>
       <Header>
@@ -37,6 +95,57 @@ const Post = ({ user, location }) => {
           </HeaderUserContainer>
         </Touchable>
       </Header>
+      <Swiper
+        showsPagination={false}
+        style={{ height: constants.height / 2.5 }}
+      >
+        {files.map(file => (
+          <Image
+            style={{ width: constants.width, height: constants.height / 2.5 }}
+            key={file.id}
+            source={{ uri: file.url }}
+          />
+        ))}
+      </Swiper>
+      <InfoContainer>
+        <IconsContainer>
+          <Touchable onPress={handleLike}>
+            <IconContainer>
+              <Ionicons
+                size={24}
+                color={isLiked ? styles.redColor : styles.blackColor}
+                name={
+                  Platform.OS === "ios"
+                    ? isLiked
+                      ? "ios-heart"
+                      : "ios-heart-empty"
+                    : isLiked
+                    ? "md-heart"
+                    : "md-heart-empty"
+                }
+              />
+            </IconContainer>
+          </Touchable>
+          <Touchable>
+            <IconContainer>
+              <Ionicons
+                color={styles.blackColor}
+                size={24}
+                name={Platform.OS === "ios" ? "ios-text" : "md-text"}
+              />
+            </IconContainer>
+          </Touchable>
+        </IconsContainer>
+        <Touchable>
+          <Bold>{likeCount === 1 ? "1 like" : `${likeCount} likes`}</Bold>
+        </Touchable>
+        <Caption>
+          <Bold>{user.username}</Bold> {caption}
+        </Caption>
+        <Touchable>
+          <CommentCount>See all {comments.length} comments</CommentCount>
+        </Touchable>
+      </InfoContainer>
     </Container>
   );
 };
