d6ad9e3fb631ca69cf60b7f816a7b5c2a6a76f46
YunHee Lee <yunheenet@gmail.com>
2020-03-06 08:47:16 +0900

    MOD Backend서버 변경(Heroku > Lambda) : 평균 응답시간 1500ms > 200ms

--- apollo.js
+++ apollo.js
@@ -1,5 +1,5 @@
 const options = {
-  uri: "https://ggobook-backend.herokuapp.com/"
+  uri: "https://491mtg7xw0.execute-api.ap-northeast-2.amazonaws.com/staging/"
   //uri: "http://192.168.0.6:4000"
 };
 
