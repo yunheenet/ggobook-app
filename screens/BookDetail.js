861dc4cd1965f0153a009cd9be405fade42b9019
YunHee Lee <yunheenet@gmail.com>
2020-03-11 12:04:24 +0900

    ADD 피드에서 책 검색 시도시 삭제된 책인 경우 에러핸들링(캐시에서 삭제)

--- screens/BookDetail.js
+++ screens/BookDetail.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { gql } from "apollo-boost";
-import { useQuery } from "@apollo/react-hooks";
+import { useQuery, useApolloClient } from "@apollo/react-hooks";
 import Loader from "../components/Loader";
 import BookCard from "../components/BookCard";
 import { View, ScrollView } from "react-native";
@@ -27,18 +27,56 @@ const BOOK_DETAIL = gql`
   }
 `;
 
+const BOOK_FEED = gql`
+  {
+    bookFeed {
+      id
+      caption
+      data {
+        id
+        title
+        author
+        coverLargeUrl
+      }
+      createdAt
+      user {
+        id
+        avatar
+        username
+      }
+    }
+  }
+`;
+
 export default ({ navigation, route }) => {
   const bookId = route.params.id;
+  const client = useApolloClient();
+
   const { loading, data, error } = useQuery(BOOK_DETAIL, {
-    variables: { id: bookId }
+    variables: { id: bookId },
+    fetchPolicy: "network-only",
+    onError: error => {
+      const data = client.cache.read({ query: BOOK_FEED });
+      const books = [...data.bookFeed];
+      const idx = books.findIndex(book => {
+        return book.id === bookId;
+      });
+      books.splice(idx, 1);
+      client.writeQuery({
+        query: BOOK_FEED,
+        data: {
+          ...data,
+          bookFeed: books
+        }
+      });
+      navigation.pop();
+    }
   });
 
   return (
     <ScrollView>
       {loading ? (
         <Loader />
-      ) : error ? (
-        navigation.pop()
       ) : (
         data && data.seeFullBook && <BookCard {...data.seeFullBook} />
       )}
